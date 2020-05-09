12d3c39ff9aed4bdc8ee5048830e6ab9f77898e1
Elzbieta Bratkowska <elabrat11@wp.pl>
2020-05-09 21:01:43 +0200

    changed image & made menu narrower & text edition &  hover menu

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const name = 'Ela';
+const name = "Ela";
 const age = 24;
 console.log(name);
 console.log(age);
 console.log(`Siema, nazywam sie ${name} i mam ${age} lat.`);
-alert('Siema mordeczko!');
