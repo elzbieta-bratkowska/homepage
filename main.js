74a0c0bc248936ed827cf72f57341a0278c4a392
Elzbieta Bratkowska <elabrat11@wp.pl>
2020-05-10 13:56:58 +0200

    bem & added JS - homework

--- main.js
+++ main.js
@@ -3,3 +3,6 @@ const age = 24;
 console.log(name);
 console.log(age);
 console.log(`Siema, nazywam sie ${name} i mam ${age} lat.`);
+
+const homework = document.querySelector(".lesson__homework-paragraph--js");
+homework.innerHTML = "Wszystko działa!";
