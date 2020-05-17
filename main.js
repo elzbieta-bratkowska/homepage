3a99fb7d6d8a26638e21cf78e541c604e037c8ee
Elzbieta Bratkowska <elabrat11@wp.pl>
2020-05-17 15:25:35 +0200

    homework after week 5

--- main.js
+++ main.js
@@ -1,8 +1,20 @@
-const name = "Ela";
-const age = 24;
-console.log(name);
-console.log(age);
-console.log(`Siema, nazywam sie ${name} i mam ${age} lat.`);
 
 const homework = document.querySelector(".lesson__homework-paragraph--js");
 homework.innerHTML = "Wszystko działa!";
+
+const name = "Jola";
+const age = 34;
+
+function hello(name, age) {
+    console.log(
+        `Siema, nazywam się ${name} i mam ${age} lat.`
+    );
+}
+hello(name, 24);
+
+const greet = (age, name) => {
+    console.log(
+        `Witam Cię serdecznie, nazywam się ${name} i mam ${age} lat.`
+    );
+    }
+greet(56, 'Maja');
